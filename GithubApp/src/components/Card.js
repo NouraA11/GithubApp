198a8338527df9168dd7e88f159508572dd4e9d3
Noura <alangarinoura11@gmail.com>
2023-08-07 07:50:25 +0300

    search repo implemented

--- GithubApp/src/components/Card.js
+++ GithubApp/src/components/Card.js
@@ -3,26 +3,37 @@ import { Text } from "react-native";
 import { View, StyleSheet } from "react-native";
 import FavoriteButton from "./FavoriteButton";
 import { Card as DefaultCard } from 'react-native-elements';
+import moment from "moment/moment";
 
-const Card = () => {
+const Card = ({ repo }) => {
     return (
         <DefaultCard
             containerStyle={styles.Card}>
-            <DefaultCard.Image
-                style={styles.CardImage}
-                source={{
-                    uri:
-                        'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
-                }}
-            />
             <View style={styles.FavoriteButton} >
                 <FavoriteButton />
             </View>
             <DefaultCard.Title style={styles.Title}>
-                CARD WITH DIVIDER
+                {repo.name}
             </DefaultCard.Title>
+            <View style={styles.ColumnContainer}>
+                <Text style={styles.Text}>{repo.forks_count} forks |</Text>
+                <Text style={styles.Text}>updated at: {moment(repo.updated_at).format('DD/MM/YYYY')}</Text>
+            </View>
+            <View style={styles.UserDetailsContainer}>
+                <DefaultCard.Image
+                    style={styles.ProfileImage}
+                    source={{
+                        uri:
+                            repo.owner.avatar_url || 'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
+                    }}
+                />
+                <View >
+                    <Text style={{ color: 'black' }}>{repo.owner.login}</Text>
+                    <Text style={styles.Text}>{repo.owner.type}</Text>
+                </View>
+            </View>
             <Text style={styles.Body}>
-                When searching for repositories, you can get text match metadata for the name and description fields when you pass the text-match media type.
+                {repo.description || 'No description available'}
             </Text>
         </DefaultCard>
     );
@@ -32,10 +43,11 @@ export default Card;
 
 const styles = StyleSheet.create({
     Card: {
-        width: '95%',
-        paddingTop: 0,
-        paddingHorizontal: 0,
+        // width: '95%',
+        paddingTop: 10,
+        paddingHorizontal: 10,
         marginTop: 0,
+        marginBottom: 10,
         borderRadius: 10,
         backgroundColor: "#fff",
         shadowColor: '#000',
@@ -44,28 +56,42 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
         elevation: 5,
     },
-    CardImage: {
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
+    ProfileImage: {
+        margin: 5,
+        height: 40,
+        width: 40,
+        borderRadius: 40,
+    },
+    UserDetailsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     FavoriteButton: {
         position: 'absolute',
         top: 0,
         right: 0,
-        padding: 10,
-        // backgroundColor: 'white',
-        // opacity: 0.3,
-        // borderBottomLeftRadius: 20, //--
+        padding: 8,
+        backgroundColor: 'lightgrey',
+        borderRadius: 50,
+    },
+    ColumnContainer: {
+        flexDirection: 'row',
     },
     Title: {
-        marginTop: 5,
         fontWeight: 'bold',
-        // color: ''
+        color: 'darkblue',
         fontSize: 18,
+        textAlign: 'left',
+        marginTop: 5,
     },
     Body: {
-        fontSize: 14,
-        margin: 5,
+        fontSize: 16,
+        color: 'black',
+        marginVertical: 10,
     },
+    Text: {
+        color: 'grey',
+        marginEnd: 5,
+    }
 
 })
--- GithubApp/src/screens/Home.js
+++ GithubApp/src/screens/Home.js
@@ -1,15 +1,58 @@
-import React from "react";
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import Card from '../components/Card';
+import useRepos from "../hooks/useRepos";
+import useSearch from "../hooks/useSearch";
 
 const Home = () => {
+    const [query, setQuery] = useState('');
+
+    const onSuccess = (data) => {
+        console.log('Successfully fetched')
+    }
+    const onError = (error) => {
+        console.log('Error encountered', error.message)
+    }
+
+    const handleSearch = (value) => {
+        setQuery(value)
+        refetch()
+    }
+
+    const handleLoadMore = () => {
+        if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }
+
+    const { data, isLoading, isError, error, isFetching, refetch, isRefetching, fetchNextPage, hasNextPage, isFetchingNextPage } = useSearch(query);
 
     return (
         <View style={styles.MainContainer}>
-            <SearchBar />
-            <Card />
-        </View>
+            <SearchBar onChangeText={handleSearch} />
+            <ScrollView onScrollEndDrag={handleLoadMore}>
+                {isLoading || isFetching ?
+                    <ActivityIndicator />
+                    : isError ?
+                        <Text>{error.message}</Text>
+                        : data?.pages.map((page) =>
+                            page.data.items.map((repo) => {
+                                return (
+                                    <View key={repo.id}>
+                                        <Card repo={repo} />
+                                    </View>
+                                )
+                            })
+
+
+                        )
+                }
+            </ScrollView>
+            {
+                isFetchingNextPage ? <ActivityIndicator /> : null
+            }
+        </View >
     );
 }
 
@@ -21,5 +64,5 @@ const styles = StyleSheet.create({
         padding: 5,
         backgroundColor: 'white',
         alignItems: 'center'
-    }
+    },
 })
