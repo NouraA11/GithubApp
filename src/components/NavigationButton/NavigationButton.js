d4d377ac716b5d8e567a94ebf7caf3b23f594f91
Noura <alangarinoura11@gmail.com>
2023-08-15 14:07:51 +0300

    refactor: screen name

--- src/components/NavigationButton/NavigationButton.js
+++ src/components/NavigationButton/NavigationButton.js
@@ -2,12 +2,14 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity } from 'react-native';
 import { Icon } from "react-native-elements";
+import { screenNames } from "../../utils/constants";
 
 const NavigationButton = () => {
     const navigation = useNavigation();
+    const {favorite} = screenNames;
 
     const handleClick = () => {
-        navigation.navigate('Favorites');
+        navigation.navigate(favorite);
     }
 
     return (
