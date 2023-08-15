f7f94097312e875b6d09c4167be3cf005b840054
Noura <alangarinoura11@gmail.com>
2023-08-15 14:10:41 +0300

    refactor: extract anonymous function

--- src/components/RepoList/RepoList.js
+++ src/components/RepoList/RepoList.js
@@ -3,12 +3,13 @@ import {FlatList, ActivityIndicator} from 'react-native';
 import Card from '../../components/Card/Card';
 
 const renderItem = ({ item }) => <Card repo={item} />
+const keyExtractor = (item, index) => `${index}-${item.id}`
 
 const RepoList = ({ query, initialDataItems, searchDataItems, isFetchingNextPage, handleLoadMore }) => {
 
     return (
         <FlatList
-            keyExtractor={(item, index) => `${index}-${item.id}`}
+            keyExtractor={keyExtractor}
             data={!query ? initialDataItems : searchDataItems}
             renderItem={renderItem}
             initialNumToRender={20}
