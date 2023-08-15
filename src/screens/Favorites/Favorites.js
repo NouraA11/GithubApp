2e5203ebe4f68f5c491901a3154f6e4878aac61e
Noura <alangarinoura11@gmail.com>
2023-08-15 14:53:03 +0300

    refactor: components

--- src/screens/Favorites/Favorites.js
+++ src/screens/Favorites/Favorites.js
@@ -4,21 +4,21 @@ import { useSelector } from "react-redux";
 import styles from './Favorites.style';
 import Card from "../../components/Card/Card";
 
+const FavoriteListComponent = () => (
+    favoriteList?.map((repo) => {
+    return (
+        <View key={repo.id}>
+            <Card repo={repo} />
+        </View>
+    )
+}))
+
+const NoFavorites = () => (
+<Text style={styles.text}>You have no favorite repositories</Text>
+)
+
 const Favorites = () => {
     const favoriteList = useSelector((state) => state.Favorite);
-    
-    const FavoriteListComponent = () => (
-        favoriteList?.map((repo) => {
-        return (
-            <View key={repo.id}>
-                <Card repo={repo} />
-            </View>
-        )
-    }))
-
-    const NoFavorites = () => (
-    <Text style={styles.text}>You have no favorite repositories</Text>
-    )
 
     return (
         <ScrollView style={styles.mainContainer}>
